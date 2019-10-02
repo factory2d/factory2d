8b7563cd001ac562df4585806c4cd3c53e009092
Alexandre Ribeiro de Sá <alexribeirodesa@gmail.com>
2019-10-02 05:51:54 -0300

    how our input manager will work

--- core/InputManager/InputManager.h
+++ core/InputManager/InputManager.h
@@ -13,6 +13,22 @@
 #ifndef FACTORY2D_INPUTMANAGER_INPUTMANAGER_H_
 #define FACTORY2D_INPUTMANAGER_INPUTMANAGER_H_
 
+/*
+a little about my UTools (https://pt.slideshare.net/alexrdes/u-tools-unite-brasil-42578443)
+
+- input manager
+  - player
+    - x [typeAxis]
+	  - joystick X
+	  - keyboard button +/-
+    - y [typeAxis]
+	  - joystick Y
+	  - keyboard button +/-
+	- attack
+	  - joystick button
+	  - keyboard button
+*/
+
 class InputManager {
 public:
 	InputManager();
